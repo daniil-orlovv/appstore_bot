77ebc596b547a19c37c81a72908a7d4aa1d2496a
Даниил Орлов <danilorlov9898@yandex.ru>
2024-04-18 16:38:48 +0300

    Added removing apps from db.

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -4,6 +4,7 @@ from aiogram.types import Message
 from sqlalchemy.orm import sessionmaker
 
 from utils.utils_db import create_app_for_db
+from models.models import App
 
 router = Router()
 
@@ -24,21 +25,30 @@ async def add(message: Message, db):
     session.add(object_for_db)
     session.commit()
 
-    await message.answer(f"Вы вызвали команду {cmd} с аргументами: {url} {title} {launch_url}")
+    await message.answer(f"Приложение {title} добавлено для мониторинга.")
 
 
 @router.message(Command('remove'))
-async def remove(message: Message):
+async def remove(message: Message, db):
     '''Удаляет URL приложения для мониторинга.'''
 
-    pass
+    connection = db.connect()
+    Session = sessionmaker(bind=db)
+    session = Session(bind=connection)
+    print(session.query(App.id, App.title).all())  # Получение всех приложений
+    i = session.query(App).filter(App.title == 'Название').one()  # Удаление выбранного приложения
+    session.delete(i)
+    session.commit()
 
 
-@router.message(Command('/setinterval'))
-async def set_interval(message: Message):
-    '''Устанавливает интервал времени для проверки доступноси приложения.'''
+@router.message(Command('setinterval'))
+async def set_interval(message: Message, config):
+    '''Устанавливает интервал времени для проверки доступности приложения.'''
 
-    pass
+    value = message.text.split()[1]
+    config.update_interval(value)
+    await message.answer(f'Интервал времени для проверки установлен: {value} минут')
+    print(config.interval_value.minutes)
 
 
 @router.message(Command('generatekey'))
