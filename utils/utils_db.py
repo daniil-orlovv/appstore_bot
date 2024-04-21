c1796c1a406befad5706a0debe255d0b76da69f3
Даниил Орлов <danilorlov9898@yandex.ru>
2024-04-21 15:44:35 +0300

    Added utils for users and made refactoring

--- utils/utils_db.py
+++ utils/utils_db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Engine
 
-from models.models import App, Key
+from models.models import App, Key, User
 
 
 def create_app_obj_for_db(kwargs: dict):
@@ -13,19 +13,7 @@ def create_app_obj_for_db(kwargs: dict):
     return object_db
 
 
-def add_app_to_db(session: Engine, data: dict):
-    object_for_db = create_app_obj_for_db(data)
-    session.add(object_for_db)
-    session.commit()
-
-
-def remove_from_db(session: Engine, name_app: str):
-    app = session.query(App).filter(App.title == name_app).one()
-    session.delete(app)
-    session.commit()
-
-
-def check_unique_app(session: Engine, data: dict) -> bool:
+def check_exist_app(session: Engine, data: dict) -> bool:
     title = data['title']
     url = data['url']
     launch_url = data['launch_url']
@@ -39,10 +27,16 @@ def check_unique_app(session: Engine, data: dict) -> bool:
         return True
 
 
-def check_key_access(session: Engine, data: str):
-    title = data
-    check_key = session.query(Key).filter(Key.title == title).all()
-    return False if check_key else True
+def remove_app_from_db(session: Engine, name_app: str):
+    app = session.query(App).filter(App.title == name_app).one()
+    session.delete(app)
+    session.commit()
+
+
+def add_app_to_db(session: Engine, data: dict):
+    object_for_db = create_app_obj_for_db(data)
+    session.add(object_for_db)
+    session.commit()
 
 
 def create_key_obj_for_db(key: str):
@@ -53,7 +47,50 @@ def create_key_obj_for_db(key: str):
     return object_db
 
 
+def check_exist_key(session: Engine, data: dict):
+    title = data
+    check_key = session.query(Key).filter(Key.title == title).all()
+    return False if check_key else True
+
+
 def add_key_to_db(session: Engine, key: str):
     object_for_db = create_key_obj_for_db(key)
     session.add(object_for_db)
     session.commit()
+
+
+def remove_key_from_db(session: Engine, key: str):
+    key_for_deleting = session.query(Key).filter(Key.title == key).one()
+    session.delete(key_for_deleting)
+    session.commit()
+
+
+def create_user_object_for_db(data: dict):
+    object_db = User(
+        id_telegram=data['id_telegram'],
+        name=data['name']
+    )
+    return object_db
+
+
+def check_exist_user(session: Engine, id_telegram: int):
+    users = session.query(User).filter(User.id_telegram == id_telegram).all()
+    if users:
+        return True
+    return False
+
+
+def add_user_to_db(session: Engine, data: dict):
+    object_for_db = create_user_object_for_db(data)
+    session.add(object_for_db)
+    session.commit()
+
+
+def get_ids_users_from_db(session: Engine):
+    q = session.query(User)
+    ids_users = [user.id_telegram for user in q]
+    return ids_users
+
+
+def check_access_for_user(session: Engine, key: str):
+    return session.query(Key).filter(Key.title == key).all()
