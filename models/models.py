77b19c0ebf894695a2fc225ebfe0656b2a9e6882
Даниил Орлов <danilorlov9898@yandex.ru>
2024-04-22 15:37:11 +0300

    Added default=0 in counter in App

--- models/models.py
+++ models/models.py
@@ -18,16 +18,10 @@ class Key(Base):
     title = Column(String())
 
 
-class KeyUser(Base):
-    __tablename__ = 'keysusers'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    key_id = Column(Integer, ForeignKey('keys.id'))
-    user_id = Column(Integer, ForeignKey('users.id'))
-
-
 class App(Base):
     __tablename__ = 'apps'
     id = Column(Integer, primary_key=True)
     title = Column(String(), unique=True)
     url = Column(String(), unique=True)
     launch_url = Column(String(), unique=True)
+    counter = Column(Integer(), default=0)
